4602c9c30b489dfcd91acf8b78c2781f09444539
anggano <88186542+anggano@users.noreply.github.com>
2021-08-02 05:59:32 +0700

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -10,7 +10,7 @@ function loadTeam(kelompok){
 	team_lap_file = teamDesc[kelompok][2];
 	team_ppt_file = teamDesc[kelompok][3];
     for (var i=0; i<=kelompok; i++) {
-		detailTeam = "<H3>PENULIS</H3>" + "<ul>";
+		detailTeam = "<H3>PENULISS</H3>" + "<ul>";
 
 	 	jum_team = anggota[kelompok].length;
 
